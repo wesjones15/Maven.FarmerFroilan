ccf9560d2596c8cc442a531c152e9e5b23a67604
kshields412 <kshields412@gmail.com>
2019-11-19 16:18:01 -0500

    working on farm

--- src/main/java/com/zipcodewilmington/froilansfarm/farm/Farm.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/farm/Farm.java
@@ -2,29 +2,40 @@ package com.zipcodewilmington.froilansfarm.farm;
 
 import com.zipcodewilmington.froilansfarm.animal.Animal;
 import com.zipcodewilmington.froilansfarm.animal.animalkinds.Chicken;
+import com.zipcodewilmington.froilansfarm.vehiclekinds.CropDuster;
+import com.zipcodewilmington.froilansfarm.vehiclekinds.Tractor;
 import com.zipcodewilmington.froilansfarm.warehouses.Container;
 import com.zipcodewilmington.froilansfarm.warehouses.containerkinds.ChickenCoop;
 import com.zipcodewilmington.froilansfarm.warehouses.containerkinds.FarmHouse;
+import com.zipcodewilmington.froilansfarm.warehouses.containerkinds.Field;
 import com.zipcodewilmington.froilansfarm.warehouses.containerkinds.Stable;
 
 import java.util.ArrayList;
 
 public class Farm {
-    FarmHouse farmHouse;
-    ArrayList<ChickenCoop> chickenCoops;
-    ArrayList<Stable> stables;
+    private Tractor tractor;
+    private CropDuster cropDuster;
+    private Field field;
+//    private Silo silo;
+    private FarmHouse farmHouse;
+    private ArrayList<ChickenCoop> chickenCoops;
+    private ArrayList<Stable> stables;
+
+    public Farm(Tractor tractor, CropDuster cropDuster, Field field, FarmHouse farmHouse, ArrayList<ChickenCoop> chickenCoops, ArrayList<Stable> stables){
+        this.tractor = tractor;
+        this.cropDuster = cropDuster;
+        this.field = field;
+//        this.silo = silo;
+        this.farmHouse = farmHouse;
+        this.chickenCoops = chickenCoops;
+        this.stables = stables;
+    }
 
     public Farm() {
-        farmHouse = new FarmHouse();
-        chickenCoops = new ArrayList<ChickenCoop>();
-        stables = new ArrayList<Stable>();
-        for (int i = 0; i < 4; i++) {
-            chickenCoops.add(new ChickenCoop());
-        }
-        for (int i = 0; i < 3; i++) {
-            stables.add(new Stable());
-        }
+        this(new Tractor(), new CropDuster(), new Field(), new FarmHouse(), new ArrayList<ChickenCoop>(), new ArrayList<Stable>());
     }
+
+
 
 
 
