6dbd420d775101495df00f82a9728b5ca8674285
wes <wes.jones1597@gmail.com>
2019-11-19 16:16:41 -0500

    add Silo Container<Edible> and associated tests

--- src/main/java/com/zipcodewilmington/froilansfarm/animal/animalkinds/Chicken.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/animal/animalkinds/Chicken.java
@@ -1,11 +1,12 @@
 package com.zipcodewilmington.froilansfarm.animal.animalkinds;
 
 import com.zipcodewilmington.froilansfarm.animal.Animal;
+import com.zipcodewilmington.froilansfarm.interfaces.Edible;
 import com.zipcodewilmington.froilansfarm.interfaces.Produce;
 
 public class Chicken extends Animal implements Produce {
 
-    public Double yield() {
+    public Edible yield() {
         return null;
     }
 
