cc40cac61f6a0e51e406d755807a78dd1a285e21
Kievina <56592501+Kievina@users.noreply.github.com>
2019-11-20 06:16:42 -0500

    add method body to vehicle classes

--- src/main/java/com/zipcodewilmington/froilansfarm/vehiclekinds/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/vehiclekinds/CropDuster.java
@@ -1,16 +1,46 @@
 package com.zipcodewilmington.froilansfarm.vehiclekinds;
 
+import com.zipcodewilmington.froilansfarm.farm.Farm;
+import com.zipcodewilmington.froilansfarm.farm.field.Crop;
 import com.zipcodewilmington.froilansfarm.interfaces.AirCraft;
 import com.zipcodewilmington.froilansfarm.interfaces.Vehicle;
+import com.zipcodewilmington.froilansfarm.peoplekinds.Pilot;
 import com.zipcodewilmington.froilansfarm.vehiclekinds.FarmVehicle;
+import com.zipcodewilmington.froilansfarm.warehouses.Container;
+import com.zipcodewilmington.froilansfarm.warehouses.containerkinds.CropRow;
+import com.zipcodewilmington.froilansfarm.warehouses.containerkinds.Field;
+
+import java.util.ArrayList;
 
 public class CropDuster extends FarmVehicle implements AirCraft, Vehicle {
+    private Pilot pilot;
+    private Farm farm;
+    private ArrayList<CropRow> field;
 
-    public String makeNoise() {
-        return "Neowww";
+    public CropDuster(Farm farm) {
+        this.farm = farm;
     }
 
-    public void fly() {
+    public String makeNoise() {
+        return "Whir whir!";
+    }
 
+    public void operate(Farm farm) {
+        fly();
+    }
+
+    //TODO
+    public void fly() {
+        // for each CropRow in field
+        for (CropRow row : field) { //need getter: farm.getfield()
+//            fertilize(row);
+        }
+    }
+
+    public void fertilize(ArrayList<Crop> cropRow) {
+        // for each crop in a CropRow
+        for (Crop crop : cropRow) {
+            crop.setHasBeenFertilized(true);
+        }
     }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/vehiclekinds/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/vehiclekinds/Tractor.java
@@ -1,11 +1,38 @@
 package com.zipcodewilmington.froilansfarm.vehiclekinds;
 
+import com.zipcodewilmington.froilansfarm.farm.Farm;
+import com.zipcodewilmington.froilansfarm.farm.field.Crop;
 import com.zipcodewilmington.froilansfarm.interfaces.Vehicle;
+import com.zipcodewilmington.froilansfarm.peoplekinds.Farmer;
 import com.zipcodewilmington.froilansfarm.vehiclekinds.FarmVehicle;
+import com.zipcodewilmington.froilansfarm.warehouses.containerkinds.CropRow;
+
+import java.util.ArrayList;
 
 public class Tractor extends FarmVehicle implements Vehicle {
+    Farmer farmer;
+    Farm farm;
+    ArrayList<CropRow> field;
+
+    public Tractor(Farm farm) {
+        this.farm = farm;
+    }
 
     public String makeNoise() {
         return "Brrrn";
+    }
+
+    //TODO
+    public void operate(Farm farm) {
+        // for each croprow in field in farm
+        for (CropRow row : field) { // farm.getField().getCropRows()) {
+//            harvest(row);
+        }
+    }
+
+    public void harvest(ArrayList<Crop> cropRow) {
+        for (Crop crop : cropRow) {
+            crop.setHasBeenHarvested(true);
+        }
     }
 }
